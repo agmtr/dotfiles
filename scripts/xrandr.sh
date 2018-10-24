ea08d7b7c4730ffc85c770c56a06ce9f49765291
ab <ab@gomage.com>
2018-10-24 12:15:58 +0300

    first commit

---
+++ scripts/xrandr.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode DP-3 1920x1080_60.00
+xrandr --output DP-1 --mode 1920x1080 --primary --output DP-2 --mode 1920x1080 --right-of DP-1 --output DP-3 --mode 1920x1080_60.00 --left-of DP-1
